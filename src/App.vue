415e3e38a02a242fcf4134123feab0a3064e0e0c
Rufio2031 <BBieniek14@gmail.com>
2017-06-29 19:40:12 -0500

    Working ToDo Project v1

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,48 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <hello></hello>
+    <todo-list v-bind:todos="todos"></todo-list>
+    <create-todo v-on:create-todo="createTodo"></create-todo>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+  import TodoList from './components/TodoList'
+  import CreateTodo from './components/CreateTodo'
 
 export default {
-  name: 'app',
-  components: {
-    Hello
-  }
+    name: 'app',
+    components: {
+      TodoList,
+      CreateTodo
+    },
+    // data function avails data to the template
+    data () {
+      return {
+        todos: [{
+          title: 'Todo A',
+          project: 'Project A',
+          done: false
+        }, {
+          title: 'Todo B',
+          project: 'Project B',
+          done: true
+        }, {
+          title: 'Todo C',
+          project: 'Project C',
+          done: false
+        }, {
+          title: 'Todo D',
+          project: 'Project D',
+          done: false
+        }]
+      }
+    },
+    methods: {
+      createTodo (newTodo) {
+        this.todos.push(newTodo)
+      }
+    }
 }
 </script>
 
